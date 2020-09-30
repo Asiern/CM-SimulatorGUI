528e1d5c412ddc8631565f1aefa2edf918fb6876
Asier Nuñez <45818936+Asiern@users.noreply.github.com>
2020-09-30 12:18:16 +0200

    Update Form1.cs

--- Source/CacheMemorySimulator/CacheMemorySimulator/Form1.cs
+++ Source/CacheMemorySimulator/CacheMemorySimulator/Form1.cs
@@ -22,7 +22,7 @@ namespace CacheMemorySimulator
             var materialSkinManager = MaterialSkinManager.Instance;
             materialSkinManager.AddFormToManage(this);
             materialSkinManager.Theme = MaterialSkinManager.Themes.LIGHT;
-            materialSkinManager.ColorScheme = new ColorScheme(Primary.Blue800, Primary.Blue900, Primary.Blue500, Accent.LightBlue200, TextShade.WHITE);
+            materialSkinManager.ColorScheme = new ColorScheme(Primary.LightBlue600, Primary.LightBlue700, Primary.LightBlue300, Accent.DeepOrange100, TextShade.WHITE);
         }
 
         private void Form1_Load(object sender, EventArgs e)
